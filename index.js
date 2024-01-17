c0918e352db0a69aa5f13313e5f001ecdcf5f5d8
wachuka7 <rachaelwachuka7@gmail.com>
2024-01-17 16:26:16 +0300

    Feat: Add renderComments function

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const commentsList=document.querySelector('#comments-list')
-    const commentElement = document.querySelector('.comments')
     const userDetails=document.querySelector('#customer-details')
    
     fetch('http://localhost:3000/comments')
         .then(response=>response.json())
-        .then(comments=> {
+        .then(comments=> {console.log(comments)
             fetch('http://localhost:3000/users')
             .then(response=>response.json())
-            .then(users=> {
+            .then(users=> {console.log(users)
                     renderComments(comments,users)
-                
-            }
+            })
+            .catch(error => error('Error fetching users:', error))
+        })
+        .catch(error => console.error('Error fetching comments:', error));
+    
+    function renderComments(comments, users) {
+        comments.forEach(comment => {
+            const commentElement = document.createElement('div');
+            commentElement.classList.add('comment');
+            commentElement.innerHTML = `<p>${comment.body}</p>`;
+            commentElement.addEventListener('click', () => showUserDetails(comment, users));
+            commentsList.appendChild(commentElement);
+        });
+    }
+       
+    })
