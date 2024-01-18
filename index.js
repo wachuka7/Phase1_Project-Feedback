1e893d7204c8dbb9b09f6aee4b4f6da36772f660
wachuka7 <rachaelwachuka7@gmail.com>
2024-01-19 02:34:39 +0300

    Feat:Add Delete Function

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     
-        function showButtons(commentContainer,index) {
+        function showButtons(commentContainer) {
             const buttonsDiv = document.createElement('div');
             buttonsDiv.classList.add('options');
     
@@ -54,13 +54,15 @@ document.addEventListener("DOMContentLoaded", function () {
     
             const optionsButton = document.createElement('button');
             optionsButton.textContent = 'Options';
-            optionsButton.addEventListener('click', () => toggleOptions(buttonsDiv));
-    
+            optionsButton.addEventListener('click', () => showOptions(optionsButton, commentContainer))
+
+            const optionsDiv= document.createElement('div');
+            optionsDiv.classList.add('options');
+
             buttonsDiv.appendChild(likeButton);
             buttonsDiv.appendChild(seeUserDetailsButton);
             buttonsDiv.appendChild(optionsButton);
-    
-            // Append options div to comment container
+            buttonsDiv.appendChild(optionsDiv);
             commentContainer.appendChild(buttonsDiv);
     
             // Add 'show' class to make options div visible
@@ -80,10 +82,34 @@ document.addEventListener("DOMContentLoaded", function () {
         // Stop the event from propagating to prevent clicking the comment triggering this event
         event.stopPropagation();
     }
+    function showOptions(optionsDiv, commentContainer){
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => deleteComment(commentContainer));
 
+        const markAsReadButton = document.createElement('button');
+        markAsReadButton.textContent = 'Mark as Read';
+        markAsReadButton.addEventListener('click', () => markAsRead(commentContainer));
+
+        optionsDiv.appendChild(deleteButton);
+        optionsDiv.appendChild(markAsReadButton);
+        optionsDiv.classList.add('show');
+    }
+    function deleteComment(commentContainer) {
+        const confirmDelete = confirm("Do you want to delete this comment?");
+
+        if (confirmDelete) {
+            commentContainer.remove();
+        }
+    }
+    function markAsRead(commentContainer) {
+       
+        commentContainer.querySelector('p').add('read-comment');
+    }
     function showUserDetails() {
         const userDetails=document.querySelector('#user-details')
-        fetch(`https://jsonplaceholder.typicode.com/users/${users.Id}`)
+        fetch(`https://jsonplaceholder.typicode.com/users`)
             .then(response=>response.json())
             .then(users=>{
             userDetails.innerHTML = `
